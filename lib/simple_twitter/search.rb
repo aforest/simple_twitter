6c7d64084bfaecba035bd048fd25799838a2de13
Austin Forest <cforest@pattonboggs.com>
2012-06-18 11:35:07 -0600

    getting closer to working

--- lib/simple_twitter/search.rb
+++ lib/simple_twitter/search.rb
@@ -1,10 +1,15 @@
 module SimpleTwitter
   class Search
+    TWITTER_SEARCH= "http://search.twitter.com/search.json"
     attr_accesor (:per_page)
     def initialize
       @per_page=10
     end
     def search(query)
+      params={
+        :q=>query,
+        :rpp=>@per_page,}
+      response=HTTParty.get(TWITTER_SEARCH,:query =>params,)
     end
   end
 end
